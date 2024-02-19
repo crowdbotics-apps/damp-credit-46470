8ce29207cfbb37e4d58108fdf75f61d97900dcc0
Crowdbotics <team@crowdbotics.com>
2024-02-19 17:45:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "damp-credit-46470",
+  url: "https://damp-credit-46470.botics.co",
+  api: "https://damp-credit-46470.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
